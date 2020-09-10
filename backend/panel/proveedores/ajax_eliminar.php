de14ba4246ae1f4f554527027f59b9b706eb051b
Antony <antony_hbk@hotmail.com>
2020-09-09 19:22:03 -0500

    content commint update

--- backend/panel/proveedores/ajax_eliminar.php
+++ backend/panel/proveedores/ajax_eliminar.php
@@ -3,17 +3,18 @@
     require_once "../../connect.php";
 
     $id = $_POST['id']; 
-    $url = $_POST['url_img'];
-    $file = '../../../img/upload/productos/'.$url;
 
-    $query = "DELETE FROM `productos` WHERE `idproducto`='$id';";
+
+    $query = "DELETE FROM `proveedores` WHERE `idproveedor`='$id';";
     
    
     $result = $mysqli->query($query);
 
+    /*
     if(is_file($file)){
         unlink($file); //elimino el fichero
     }
+    */
 
 
     if(!$result){
