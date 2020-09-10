c46be931237cc4990cdb637179660fe410b1fcf7
katsuragy07 <71047899+katsuragy07@users.noreply.github.com>
2020-09-10 17:52:42 -0500

    Update ajax_enviar_correo.php

--- backend/panel/ventas/cotizaciones/ajax_enviar_correo.php
+++ backend/panel/ventas/cotizaciones/ajax_enviar_correo.php
@@ -16,7 +16,7 @@
     // Declaramos el formato del documento PDF
     $html2pdf = new HTML2PDF('P', array($pdf_x,$pdf_y), 'es', true, 'UTF-8', array(0, 0, 0, 0));
     $html2pdf->pdf->SetDisplayMode('fullpage');
-    $message = '<page><img src="'.$pdf.'"></page>';
+    $message = '<page><img src="'.$pdf.'"/></page>';
     $html2pdf->writeHTML($message);
 
 /*
